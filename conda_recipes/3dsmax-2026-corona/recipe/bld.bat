e3da20aefb447046618073cecdb598ae20e351ad
Johannes Oehmen <mail@oehmen.digital>
2025-12-02 11:17:20 +0100

    Amending 3dsmax-2026-corona receipe

--- conda_recipes/3dsmax-2026-corona/recipe/bld.bat
+++ conda_recipes/3dsmax-2026-corona/recipe/bld.bat
@@ -9,4 +9,28 @@ rem /E recursive, /I assume destination is a directory, /H copy hidden, /Y overw
 xcopy "%SRC_DIR%" "%TARGET%" /E /I /H /Y >nul
 if errorlevel 1 exit /b 1
 
+rem Add activation hooks to expose the Corona load path
+set "ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d"
+set "DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d"
+if not exist "%ACTIVATE_DIR%" mkdir "%ACTIVATE_DIR%"
+if not exist "%DEACTIVATE_DIR%" mkdir "%DEACTIVATE_DIR%"
+
+set "ACTIVATE_SH=%ACTIVATE_DIR%\%PKG_NAME%-%PKG_VERSION%-vars.sh"
+set "DEACTIVATE_SH=%DEACTIVATE_DIR%\%PKG_NAME%-%PKG_VERSION%-vars.sh"
+set "ACTIVATE_BAT=%ACTIVATE_DIR%\%PKG_NAME%-%PKG_VERSION%-vars.bat"
+set "DEACTIVATE_BAT=%DEACTIVATE_DIR%\%PKG_NAME%-%PKG_VERSION%-vars.bat"
+
+(
+  echo #!/bin/sh
+  echo export CORONA_3DSMAX_2026_LOAD_PATH=\"$(cygpath "%TARGET%")\"
+) > "%ACTIVATE_SH%"
+
+(
+  echo #!/bin/sh
+  echo unset CORONA_3DSMAX_2026_LOAD_PATH
+) > "%DEACTIVATE_SH%"
+
+echo set "CORONA_3DSMAX_2026_LOAD_PATH=%TARGET%" > "%ACTIVATE_BAT%"
+echo set CORONA_3DSMAX_2026_LOAD_PATH= > "%DEACTIVATE_BAT%"
+
 exit /b 0
