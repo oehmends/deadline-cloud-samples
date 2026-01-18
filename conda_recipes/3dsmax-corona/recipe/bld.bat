68ca5d52a048e935f28bd99b048d6fef591dfec0
Johannes Oehmen <mail@oehmen.digital>
2026-01-18 12:58:11 +0100

    Amending config

--- conda_recipes/3dsmax-corona/recipe/bld.bat
+++ conda_recipes/3dsmax-corona/recipe/bld.bat
@@ -9,9 +9,9 @@ set "TARGET=%TARGET_ROOT%\Corona Renderer for 3ds Max\%MAX_VERSION%"
 if not exist "%TARGET_ROOT%" mkdir "%TARGET_ROOT%"
 if not exist "%TARGET%" mkdir "%TARGET%"
 
-rem /E recursive, /I assume destination is a directory, /H copy hidden, /Y overwrite
-xcopy "%SRC_DIR%\*" "%TARGET_ROOT%" /E /I /H /Y >nul
-if errorlevel 1 exit /b 1
+rem Use robocopy to preserve the full Corona payload (including empty dirs).
+robocopy "%SRC_DIR%" "%TARGET_ROOT%" /E /COPY:DAT /DCOPY:DAT /R:3 /W:5 /NFL /NDL /NJH /NJS /NC /NS >nul
+if %ERRORLEVEL% GEQ 8 exit /b 1
 
 rem Add activation hooks to expose the Corona load path
 set "ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d"
